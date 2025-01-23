e33550a0fb3f21319c319780265d8b86964d0d57
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-23 14:18:04 +0530

    Updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,10 @@
-test.
+resource "aws_instance" "ec2" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "Terraform-Instance-${count.index + 1}"
+  }
+}
