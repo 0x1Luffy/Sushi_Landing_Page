7aa4d29a67e7d564178d5037bbbb9ea07890ba30
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-23 14:16:00 +0530

    Updated variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,1 +1,29 @@
+variable "aws_region" {
+  description = "AWS region to deploy the instances"
+  default     = "ap-south-1" # Default is optional
+}
 
+variable "aws_profile" {
+  description = "AWS CLI profile name"
+  default     = "default" # Optional, replace if using a custom profile
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  default     = 3
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the instances"
+  default     = "ami-04b4f1a9cf54c11d0"
+}
+
+variable "key_name" {
+  description = ""
+  default     = "chetankesare" # Replace with your actual key pair name
+}
