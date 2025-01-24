b16efca428d5cf2eff0461b9e85902570df1c2b1
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-24 10:59:20 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region = var.aws_region
+  
+  default_tags {
+    tags = {
+      ManagedBy   = "Terraform"
+      Project     = var.project_name
+    }
+  }
 }
