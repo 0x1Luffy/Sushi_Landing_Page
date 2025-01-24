36741595ac02f9f72e1877afca73ac2695abf568
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-24 11:02:04 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,24 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "servers" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
 
   tags = {
-    Name = "Terraform-Instance-${count.index + 1}"
+    Name = "${var.project_name}-server-${count.index + 1}"
   }
+}
+
+output "instance_ids" {
+  description = "List of instance IDs"
+  value       = aws_instance.servers[*].id
+}
+
+output "instance_private_ips" {
+  description = "List of private IP addresses"
+  value       = aws_instance.servers[*].private_ip
+}
+
+output "instance_public_ips" {
+  description = "List of public IP addresses"
+  value       = aws_instance.servers[*].public_ip
 }
