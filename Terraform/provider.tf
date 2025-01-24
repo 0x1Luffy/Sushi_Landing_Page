d704116934f05aaec84f7a100d11b8671f3d785d
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-24 11:30:45 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"  #change the version according to you machine for details check you aws provider version and match with this version.
     }
   }
 }
 
 provider "aws" {
   region = var.aws_region
-  
+
   default_tags {
     tags = {
       ManagedBy   = "Terraform"
